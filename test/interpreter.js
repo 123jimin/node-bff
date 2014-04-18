c6f4bd7baad35bb4abf542691c101ba041840316
Jimin Park <pjm950@gmail.com>
2014-04-18 21:11:15 +0900

    Added some tests

--- test/interpreter.js
+++ test/interpreter.js
@@ -1,4 +1,5 @@
 var assert = require('assert'),
+	async = require('async'),
 	fs = require('fs');
 var BFF = require('../'),
 	BInterp = BFF.BInterp;
@@ -62,7 +63,7 @@ describe('BIR', function(){
 			assert.doesNotThrow(BFF.parse.bind(BFF, "[[][]]"));
 			assert.doesNotThrow(BFF.parse.bind(BFF, "[][[]]"));
 		});
-		it("should line number and column number correctly when un-matched brackets are detected", function(){
+		it("should give line number and column number correctly when un-matched brackets are detected", function(){
 			assert.throws(BFF.parse.bind(BFF, "["), /line 1\, column 1/);
 			assert.throws(BFF.parse.bind(BFF, "]"), /line 1\, column 1/);
 			assert.throws(BFF.parse.bind(BFF, "[[]"), /line 1\, column 1/);
@@ -77,8 +78,8 @@ describe('BIR', function(){
 
 describe('BInterp', function(){
 	this.timeout(10);
-	var _exec_out_tester = function execute_output_tester(file, output, callback){
-		var ir = BFF.parse(files[file]);
+	var _exec_out_tester = function execute_output_tester(src, output, callback){
+		var ir = BFF.parse(src);
 		var _output = "";
 		var interp = new BInterp(function(arr, callback){
 			_output += String.fromCharCode.apply(String, arr);
@@ -95,24 +96,62 @@ describe('BInterp', function(){
 			}
 			callback();
 		});
+	}, _exec_err_tester = function execute_error_tester(src, test_regex, callback){
+		(new BInterp()).execute(BFF.parse(src), function(err){
+			try{
+				assert.ok(err instanceof Error);
+				assert.ok(test_regex.test(err.message));
+			}catch(e){
+				callback(e); return;
+			}
+			callback();
+		});
 	};
+	var _1024L = "<", _1024R = ">";
+	(function(i){
+		for(;i<10;i++){
+			_1024L += _1024L;
+			_1024R += _1024R;
+		}
+		assert.equal(_1024L.length, 1024);
+		assert.equal(_1024R.length, 1024);
+	}(0));
 	describe('execute()', function(){
-		it("should calculate 1+1 correctly", function(done){
-			(new BInterp()).execute(BFF.parse(">>>++"), function(err, arr, arr_ptr){
-				if(err){
-					done(err); return;
-				}
-				try{
-					assert.equal(arr_ptr, 3);
-					assert.equal(arr[3], 2);
-				}catch(e){
-					done(e); return;
-				}
-				done();
+		describe('error', function(){
+			it("should not allow accessing array with negative index", function(done){
+				async.waterfall([
+					_exec_err_tester.bind(null, "<+", /negative index/),
+					_exec_err_tester.bind(null, ">>-<<<-", /negative index/),
+					_exec_err_tester.bind(null, ">+<<+", /negative index/),
+					_exec_err_tester.bind(null, "<,", /negative index/),
+					_exec_err_tester.bind(null, "<.", /negative index/),
+				], done);
 			});
 		});
-		it("should execute \"Hello, world!\" program correctly", function(done){
-			_exec_out_tester("hello-world.1.bf", "Hello World!\n", done);
+		describe('compute', function(){
+			it("should calculate 1+1 correctly", function(done){
+				(new BInterp()).execute(BFF.parse(">>>++"), function(err, arr, arr_ptr){
+					if(err){
+						done(err); return;
+					}
+					try{
+						assert.equal(arr_ptr, 3);
+						assert.equal(arr[3], 2);
+					}catch(e){
+						done(e); return;
+					}
+					done();
+				});
+			});
+		});
+		describe('print', function(){
+			it("should print all 256 characters correctly", function(done){
+				for(var s="", i=0; i<256; i++) s += String.fromCharCode(i);
+				_exec_out_tester(".+[.+]", s, done);
+			});
+			it("should execute \"Hello, world!\" program correctly", function(done){
+				_exec_out_tester(files["hello-world.1.bf"], "Hello World!\n", done);
+			});
 		});
 	});
 });
