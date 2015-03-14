442f846d0767752e3aea557d310c2fdba68468ef
Jimin Park <pjm950@gmail.com>
2015-03-15 08:31:28 +0900

    Added more tests for BMIR

--- test/bf-macro.js
+++ test/bf-macro.js
@@ -27,32 +27,49 @@ var _exec_out_tester = function execute_output_tester(ir, output, callback){
 	});
 };
 
+var _bmf_to_bf = function bmf_to_bf(code){
+	return BMIR.parse(code).toBrainFuck();
+};
+
 describe('BMIR', function(){
 	this.timeout(100);
 	// actually parse and execute
 	describe("parse and execute", function(){
-		it("should execute simple program with plus instructions correctly", function(done){
-			var source = files["hello.1.bfm"].trim(),
-				bmir = BMIR.parse(source);
-			_exec_out_tester(bmir.toBIR(), "hello", done);
-		});
-		it("should execute program computing division operation correctly", function(done){
-			var source = files["divide.1.bfm"].trim(),
-				bmir = BMIR.parse(source);
-
-			(new BInterp()).execute(bmir.toBIR(), function(err, arr, arr_ptr){
-				if(err){
-					done(err); return;
-				}
-				try{
-					assert.equal(arr[0], 0);
-					assert.equal(arr[1], 0);
-					assert.equal(arr[2], 0|47/9);
-					assert.equal(arr[3], 47%9);
-				}catch(e){
-					done(e); return;
-				}
+		describe("pointer position", function(){
+			it("should compute position of pointers correctly", function(done){
+				assert.equal(_bmf_to_bf("V=4;to(X)++to(Y)--<to(V).to(Z),"), "++>--<>>>>.<<,");
+				assert.equal(_bmf_to_bf("X=1;>>to(X)plus(4)[to(Y)plus(4)to(X)-]to(Z)."), ">><++++[<++++>-]>.");
 				done();
+			});
+			it("should use at() while computing position of pointers", function(done){
+				assert.equal(_bmf_to_bf("X=3;Y=7;to(Y)at(X)to(0)"), ">>>>>>><<<");
+				done();
+			});
+		});
+		describe("general excution", function(){
+			it("should execute simple program with plus instructions correctly", function(done){
+				var source = files["hello.1.bfm"].trim(),
+					bmir = BMIR.parse(source);
+				_exec_out_tester(bmir.toBIR(), "hello", done);
+			});
+			it("should execute program computing division operation correctly", function(done){
+				var source = files["divide.1.bfm"].trim(),
+					bmir = BMIR.parse(source);
+
+				(new BInterp()).execute(bmir.toBIR(), function(err, arr, arr_ptr){
+					if(err){
+						done(err); return;
+					}
+					try{
+						assert.equal(arr[0], 0);
+						assert.equal(arr[1], 0);
+						assert.equal(arr[2], 0|47/9);
+						assert.equal(arr[3], 47%9);
+					}catch(e){
+						done(e); return;
+					}
+					done();
+				});
 			});
 		});
 	});
